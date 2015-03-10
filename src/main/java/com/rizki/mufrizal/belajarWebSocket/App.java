67477bb51c6f7798357eb72413c68e906b9fc0c5
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-03-10 21:23:57 +0700

    Initial Project

---
+++ src/main/java/com/rizki/mufrizal/belajarWebSocket/App.java
@@ -0,0 +1,13 @@
+package com.rizki.mufrizal.belajarWebSocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
