61dbbf43b4cbd38ba8e0d3e1c5e61c6154c9615a
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-03-11 09:33:52 +0700

    Update Config Web Initializer

--- src/main/java/com/rizki/mufrizal/belajarWebSocket/config/WebAppInitializer.java
+++ src/main/java/com/rizki/mufrizal/belajarWebSocket/config/WebAppInitializer.java
@@ -1,7 +1,50 @@
 package com.rizki.mufrizal.belajarWebSocket.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletRegistration;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Created by rizki on 10/03/15.
  */
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setInitParameter("dispatchOptionsRequest", "true");
+        registration.setAsyncSupported(true);
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {
+            WebSocketConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+            WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{
+            "/"
+        };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding(StandardCharsets.UTF_8.name());
+        return new Filter[]{
+            characterEncodingFilter
+        };
+    }
 }
