43ee98f3655575e5b26640ab0acb0b7399feaa19
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-03-11 09:23:08 +0700

    Update Config Thymeleaf

--- src/main/java/com/rizki/mufrizal/belajarWebSocket/config/WebConfig.java
+++ src/main/java/com/rizki/mufrizal/belajarWebSocket/config/WebConfig.java
@@ -1,9 +1,14 @@
 package com.rizki.mufrizal.belajarWebSocket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 /**
  * Created by rizki on 10/03/15.
@@ -12,8 +17,55 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.rizki.mufrizal.belajarWebSocket"})
+@Import(value = {WebSocketConfig.class})
 public class WebConfig extends WebMvcConfigurerAdapter{
 
+    @Bean
+    public WebContentInterceptor webContentInterceptor(){
+        WebContentInterceptor contentInterceptor = new WebContentInterceptor();
+        contentInterceptor.setCacheSeconds(0);
+        contentInterceptor.setUseExpiresHeader(true);
+        contentInterceptor.setUseCacheControlHeader(true);
+        contentInterceptor.setUseCacheControlNoStore(true);
+        return contentInterceptor;
+    }
 
+    @Bean
+    public ServletContextTemplateResolver servletContextTemplateResolver(){
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/pages/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
 
+    @Bean
+    public SpringTemplateEngine springTemplateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(servletContextTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(springTemplateEngine());
+        return viewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer defaultServletHandlerConfigurer){
+        defaultServletHandlerConfigurer.enable();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(webContentInterceptor());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/lib/**").addResourceLocations("/lib/");
+    }
 }
