40f35f6dd2ca00fc32a87e98809e990c642333b7
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-03-11 09:23:28 +0700

    Add Web Socket Config

---
+++ src/main/java/com/rizki/mufrizal/belajarWebSocket/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.rizki.mufrizal.belajarWebSocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * Created by rizki on 10/03/15.
+ */
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+
+    }
+}
